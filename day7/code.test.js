ea855fd02f0c1ae67e4e1977bbdc046a90597233
Jeremy Casey <jeremy8883@gmail.com>
2021-12-06 23:12:06 +1100

    Prep day 7

---
+++ day7/code.test.js
@@ -0,0 +1,17 @@
+const { runChallengeA, runChallengeB } = require("./code");
+
+const mockInput = [];
+
+describe("Day 7: runChallengeA", () => {
+  it("gets the results", () => {
+    const result = runChallengeA(mockInput);
+    expect(result).toEqual("TODO");
+  });
+});
+
+describe("Day 7: runChallengeB", () => {
+  it("gets the results", () => {
+    const result = runChallengeB(mockInput);
+    expect(result).toEqual("TODO");
+  });
+});
