69cfda9379b1f05e930b33e484e6a36de1e82683
Jeremy Casey <jeremy8883@gmail.com>
2021-12-10 12:50:58 +1100

    Day 10 init

--- day10/code.test.js
+++ day10/code.test.js
@@ -1,20 +1,15 @@
----
-to: day<%=dayNumber%>/code.test.js
----
 import { runChallengeA, runChallengeB, parseInput } from "./code.js";
 
-const mockInput = parseInput(
-  ``
-);
+const mockInput = parseInput(``);
 
-describe("Day <%=dayNumber%>: runChallengeA", () => {
+describe("Day 10: runChallengeA", () => {
   it("gets the results", () => {
     const result = runChallengeA(mockInput);
     expect(result).toEqual("TODO");
   });
 });
 
-describe("Day <%=dayNumber%>: runChallengeB", () => {
+describe("Day 10: runChallengeB", () => {
   it("gets the results", () => {
     const result = runChallengeB(mockInput);
     expect(result).toEqual("TODO");
