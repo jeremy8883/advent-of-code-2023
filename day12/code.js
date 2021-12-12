0f753db27b2d85847296d7fb57450325f556f239
Jeremy Casey <jeremy8883@gmail.com>
2021-12-12 22:46:15 +1100

    Day 12 Pt 1

--- day12/code.js
+++ day12/code.js
@@ -1,10 +1,49 @@
 import R from "ramda";
 
-export const parseInput = (str) => str.split("\n");
+export const parseInput = (str) =>
+  str.split("\n").reduce((acc, line) => {
+    const [a, b] = line.split("-");
+    return {
+      ...acc,
+      [a]: acc[a] ? [...acc[a], b] : [b],
+      [b]: acc[b] ? [...acc[b], a] : [a],
+    };
+  }, {});
 
-export const runChallengeA = (input) => {
-  const result = "TODO";
-  return result;
+const isUpperCase = (val) => val === val.toUpperCase();
+
+const findPaths = (
+  caveLinkMap,
+  path = ["start"],
+  visited = new Set(["start"])
+) => {
+  const cave = R.last(path);
+  const linkedCaves = caveLinkMap[cave];
+
+  if (cave === "end") {
+    return [path];
+  } else {
+    const nextCaves = linkedCaves.filter(
+      (linkedCave) => isUpperCase(linkedCave) || !visited.has(linkedCave)
+    );
+    if (nextCaves.length === 0) {
+      return [];
+    } else {
+      return nextCaves
+        .flatMap((nextCave) =>
+          findPaths(
+            caveLinkMap,
+            [...path, nextCave],
+            new Set([...visited, nextCave])
+          )
+        )
+        .filter((p) => p.length > 0);
+    }
+  }
+};
+
+export const runChallengeA = (caveLinkMap) => {
+  return findPaths(caveLinkMap).length;
 };
 
 export const runChallengeB = (input) => {
--- day12/code.test.js
+++ day12/code.test.js
@@ -1,19 +1,54 @@
 import { runChallengeA, runChallengeB, parseInput } from "./code.js";
 
-const mockInput = parseInput(
-  ``
+const mockInput1 = parseInput(
+  `dc-end
+HN-start
+start-kj
+dc-start
+dc-HN
+LN-dc
+HN-end
+kj-sa
+kj-HN
+kj-dc`
+);
+
+const mockInput2 = parseInput(
+  `fs-end
+he-DX
+fs-he
+start-DX
+pj-DX
+end-zg
+zg-sl
+zg-pj
+pj-he
+RW-he
+fs-DX
+pj-RW
+zg-RW
+start-pj
+he-WI
+zg-he
+pj-fs
+start-RW`
 );
 
 describe("Day 12: runChallengeA", () => {
   it("gets the results", () => {
-    const result = runChallengeA(mockInput);
-    expect(result).toEqual("TODO");
+    const result = runChallengeA(mockInput1);
+    expect(result).toEqual(19);
+  });
+
+  it("gets the result for a larger set", () => {
+    const result = runChallengeA(mockInput2);
+    expect(result).toEqual(226);
   });
 });
 
 describe("Day 12: runChallengeB", () => {
   it("gets the results", () => {
-    const result = runChallengeB(mockInput);
+    const result = runChallengeB(mockInput1);
     expect(result).toEqual("TODO");
   });
 });
