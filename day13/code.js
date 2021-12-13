33319fc34f1791864178f7fd5144a2634f7397fd
Jeremy Casey <jeremy8883@gmail.com>
2021-12-13 18:31:01 +1100

    Day 13, A

--- day13/code.js
+++ day13/code.js
@@ -1,10 +1,54 @@
 import R from "ramda";
 
-export const parseInput = (str) => str.split("\n");
+export const parseInput = (str) => {
+  const lines = str.split("\n");
+
+  const points = lines
+    .filter((l) => l && !l.startsWith("fold"))
+    .map((l) => l.split(","))
+    .map(([x, y]) => ({ x: Number(x), y: Number(y) }));
+
+  const folds = lines
+    .filter((l) => l && l.startsWith("fold"))
+    .map((l) => l.replace("fold along", "").split("="))
+    .map(([axis, value]) => ({ axis: axis.trim(), value: Number(value) }));
+
+  return {
+    points,
+    folds,
+  };
+};
+
+const makeFold = ({ axis, value }, point) => {
+  if (axis === "x") {
+    if (point.x < value) {
+      return point;
+    } else {
+      return {
+        ...point,
+        x: point.x - 2 * (point.x - value),
+      };
+    }
+  } else {
+    if (point.y < value) {
+      return point;
+    } else {
+      return {
+        ...point,
+        y: point.y - 2 * (point.y - value),
+      };
+    }
+  }
+};
 
 export const runChallengeA = (input) => {
-  const result = "TODO";
-  return result;
+  return R.pipe(
+    R.map((point) => {
+      return makeFold(input.folds[0], point);
+    }),
+    R.uniqBy((point) => `${point.x},${point.y}`),
+    R.prop("length")
+  )(input.points);
 };
 
 export const runChallengeB = (input) => {
