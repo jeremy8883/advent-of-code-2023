ee883db2e79000c5ac91e31d244e3e507e231002
Jeremy Casey <jeremy8883@gmail.com>
2021-12-15 21:39:37 +1100

    Day 15

---
+++ day15/code.js
@@ -0,0 +1,13 @@
+import R from "ramda";
+
+export const parseInput = (str) => str.split("\n");
+
+export const runChallengeA = (input) => {
+  const result = "TODO";
+  return result;
+};
+
+export const runChallengeB = (input) => {
+  const result = "TODO";
+  return result;
+};
