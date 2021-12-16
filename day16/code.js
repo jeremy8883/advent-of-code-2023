a3bfa4e6d643c803bc659fd91035742a4a41305a
Jeremy Casey <jeremy8883@gmail.com>
2021-12-16 23:46:15 +1100

    Day 16, Pt 1

--- day16/code.js
+++ day16/code.js
@@ -2,9 +2,114 @@ import R from "ramda";
 
 export const parseInput = (str) => str;
 
-export const runChallengeA = (input) => {
-  const result = "TODO";
-  return result;
+const hexToBin = (hexes) =>
+  hexes
+    .split("")
+    .map((hex) => parseInt(hex, 16).toString(2).padStart(4, "0"))
+    .join("");
+
+const createIterator = (str, initialIndex = 0) => {
+  let i = initialIndex;
+  return {
+    takeNext: (amount) => {
+      if (i + amount > str.length) {
+        throw new Error("Exceeded string length");
+      }
+
+      const result = str.substr(i, amount);
+      i += amount;
+      return result;
+    },
+    hasNext: () => i < str.length,
+    getCurrentIndex: () => i,
+  };
+};
+
+const decodePacket = (bin, iterator) => {
+  iterator = iterator || createIterator(bin, 0);
+  const { takeNext, getCurrentIndex } = iterator;
+
+  const startIndex = getCurrentIndex();
+  const version = parseInt(takeNext(3), 2);
+  const typeId = takeNext(3);
+  const isLiteralValue = typeId === "100";
+
+  if (isLiteralValue) {
+    let literalValue = "";
+    while (true) {
+      const isLast = takeNext(1) === "0";
+      literalValue += takeNext(4);
+      if (isLast) {
+        break;
+      }
+    }
+    return {
+      version,
+      type: "literal",
+      startIndex,
+      endIndex: getCurrentIndex(),
+      value: parseInt(literalValue, 2),
+    };
+  } else {
+    const isTotalLength = takeNext(1) === "0";
+    if (isTotalLength) {
+      const lengthOfSubPackets = parseInt(takeNext(15), 2);
+      const endIndex = getCurrentIndex() + lengthOfSubPackets;
+
+      let subPackets = [];
+      while (true) {
+        const subPacket = decodePacket(bin, iterator);
+        subPackets.push(subPacket);
+        if (subPacket.endIndex === endIndex) {
+          break;
+        } else if (subPacket.endIndex > endIndex) {
+          throw new Error(
+            `Subpacket length (${subPacket.endIndex}) exceeds total length (${endIndex})`
+          );
+        }
+      }
+
+      return {
+        version,
+        type: "operator",
+        startIndex,
+        endIndex: getCurrentIndex(),
+        subPackets,
+      };
+    } else {
+      const numSubPackets = parseInt(takeNext(11), 2);
+
+      const subPackets = R.range(0, numSubPackets).map(() => {
+        return decodePacket(bin, iterator);
+      });
+
+      return {
+        version,
+        type: "operator",
+        startIndex,
+        endIndex: getCurrentIndex(),
+        subPackets,
+      };
+    }
+  }
+};
+
+const sumVersions = (packet) => {
+  if (packet.type === "literal") {
+    return packet.version;
+  } else {
+    return (
+      packet.subPackets.reduce((acc, subPacket) => {
+        return acc + sumVersions(subPacket);
+      }, 0) + packet.version
+    );
+  }
+};
+
+export const runChallengeA = (hex) => {
+  const bin = hexToBin(hex);
+
+  return R.pipe(decodePacket, sumVersions)(bin);
 };
 
 export const runChallengeB = (input) => {
--- day16/code.test.js
+++ day16/code.test.js
@@ -1,19 +1,21 @@
 import { runChallengeA, runChallengeB, parseInput } from "./code.js";
 
-const mockInput = parseInput(
-  ``
-);
-
 describe("Day 16: runChallengeA", () => {
   it("gets the results", () => {
-    const result = runChallengeA(mockInput);
-    expect(result).toEqual("TODO");
+    expect(runChallengeA(parseInput(`8A004A801A8002F478`))).toEqual(16);
+    expect(runChallengeA(parseInput(`620080001611562C8802118E34`))).toEqual(12);
+    expect(runChallengeA(parseInput(`C0015000016115A2E0802F182340`))).toEqual(
+      23
+    );
+    expect(runChallengeA(parseInput(`A0016C880162017C3686B18A3D4780`))).toEqual(
+      31
+    );
   });
 });
 
 describe("Day 16: runChallengeB", () => {
   it("gets the results", () => {
-    const result = runChallengeB(mockInput);
+    const result = runChallengeB("");
     expect(result).toEqual("TODO");
   });
 });
