d6a9748e1d4f4e0dd6b67844e8aa74780a99033a
Jeremy Casey <jeremy8883@gmail.com>
2021-12-17 22:32:55 +1100

    Day 17, Pt 1

--- day17/code.test.js
+++ day17/code.test.js
@@ -1,13 +1,11 @@
 import { runChallengeA, runChallengeB, parseInput } from "./code.js";
 
-const mockInput = parseInput(
-  ``
-);
+const mockInput = parseInput(`target area: x=20..30, y=-10..-5`);
 
 describe("Day 17: runChallengeA", () => {
   it("gets the results", () => {
     const result = runChallengeA(mockInput);
-    expect(result).toEqual("TODO");
+    expect(result).toEqual(45);
   });
 });
 
--- utils/geometry.js
+++ utils/geometry.js
@@ -6,10 +6,10 @@ export const newRect = (x, y, width, height) => ({
 });
 
 export const newRectByPoints = (x, y, x2, y2) => ({
-  x,
-  y,
-  width: x + x2,
-  height: y + y2,
+  x: x2 >= x ? x : x2,
+  y: y2 >= y ? y : y2,
+  width: x2 >= x ? x2 - x : x - x2,
+  height: y2 >= y ? y2 - y : y - y2,
 });
 
 export const newPoint = (x, y) => ({ x, y });
@@ -17,3 +17,15 @@ export const newPoint = (x, y) => ({ x, y });
 export const newSize = (width, height) => ({ width, height });
 
 export const pointEquals = (pA, pB) => pA.x === pB.x && pA.y === pB.y;
+
+export const addPoints = (pA, pB) => ({ x: pA.x + pB.x, y: pA.y + pB.y });
+
+export const getX2 = (rect) => rect.x + rect.width;
+
+export const getY2 = (rect) => rect.y + rect.height;
+
+export const isInside = (point, area) =>
+  point.x >= area.x &&
+  point.y >= area.y &&
+  point.x < getX2(area) &&
+  point.y < getY2(area);
