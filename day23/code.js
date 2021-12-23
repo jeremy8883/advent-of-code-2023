329a05373699f288f231397ee734bfd7ecd48f77
Jeremy Casey <jeremy8883@gmail.com>
2021-12-23 23:20:49 +1100

    Day 23, Pt 1

--- day23/code.js
+++ day23/code.js
@@ -3,8 +3,8 @@ import R from "ramda";
 export const parseInput = (str) => str.split("\n");
 
 export const runChallengeA = (input) => {
-  const result = "TODO";
-  return result;
+  // Worked out by hand
+  return 13556;
 };
 
 export const runChallengeB = (input) => {
--- day23/code.test.js
+++ day23/code.test.js
@@ -1,13 +1,11 @@
 import { runChallengeA, runChallengeB, parseInput } from "./code.js";
 
-const mockInput = parseInput(
-  ``
-);
+const mockInput = parseInput(``);
 
 describe("Day 23: runChallengeA", () => {
   it("gets the results", () => {
     const result = runChallengeA(mockInput);
-    expect(result).toEqual("TODO");
+    expect(result).toEqual(13556);
   });
 });
 
