495057d7f0132e931c946e78d82d495e1fa01771
Jeremy Casey <jeremy8883@gmail.com>
2021-12-26 12:46:42 +1100

    Day 24, Pt 1 WIP

--- day24/code.test.js
+++ day24/code.test.js
@@ -1,17 +1,39 @@
-import { runChallengeA, runChallengeB, parseInput } from "./code.js";
+import {
+  runChallengeA,
+  runChallengeB,
+  parseInput,
+  _runInstructions,
+} from "./code.js";
 
 const mockInput = parseInput(
-  ``
+  `inp w
+add z w
+mod z 2
+div w 2
+add y w
+mod y 2
+div w 2
+add x w
+mod x 2
+div w 2
+mod w 2`
 );
 
-describe("Day 24: runChallengeA", () => {
+describe("_runInstructions", () => {
+  it("runs the instruction set", () => {
+    const result = _runInstructions(mockInput, "5");
+    expect(result).toEqual({ w: 0, x: 1, y: 0, z: 1 });
+  });
+});
+
+xdescribe("Day 24: runChallengeA", () => {
   it("gets the results", () => {
     const result = runChallengeA(mockInput);
     expect(result).toEqual("TODO");
   });
 });
 
-describe("Day 24: runChallengeB", () => {
+xdescribe("Day 24: runChallengeB", () => {
   it("gets the results", () => {
     const result = runChallengeB(mockInput);
     expect(result).toEqual("TODO");
