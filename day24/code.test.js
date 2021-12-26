f9e21398aa9f8217c79882544a38b9776f78ff37
Jeremy Casey <jeremy8883@gmail.com>
2021-12-26 11:07:14 +1100

    Day 24, BP

---
+++ day24/code.test.js
@@ -0,0 +1,19 @@
+import { runChallengeA, runChallengeB, parseInput } from "./code.js";
+
+const mockInput = parseInput(
+  ``
+);
+
+describe("Day 24: runChallengeA", () => {
+  it("gets the results", () => {
+    const result = runChallengeA(mockInput);
+    expect(result).toEqual("TODO");
+  });
+});
+
+describe("Day 24: runChallengeB", () => {
+  it("gets the results", () => {
+    const result = runChallengeB(mockInput);
+    expect(result).toEqual("TODO");
+  });
+});
